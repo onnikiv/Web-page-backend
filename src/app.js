2f2b7bd09f48be23a552ce607d5a257bfc77190c
onnik <onni.kivinen@gmail.com>
2025-04-13 17:22:46 +0300

    Update CORS configuration to include additional origin

--- src/app.js
+++ src/app.js
@@ -6,7 +6,11 @@ const app = express();
 
 app.use(
   cors({
-    origin: ['http://127.0.0.1:5500', 'http://localhost:5500'],
+    origin: [
+      'http://127.0.0.1:5500',
+      'http://localhost:5500',
+      'https://10.120.32.69:3000',
+    ],
     credentials: true,
   })
 );
