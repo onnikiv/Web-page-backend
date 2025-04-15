6bd0ee5618f3be0271d7faeacb60fb5c76bcf22d
onnik <onni.kivinen@gmail.com>
2025-04-15 15:43:10 +0300

    Refactor CORS configuration to restrict origins to a single domain

--- src/app.js
+++ src/app.js
@@ -6,11 +6,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: [
-      'http://127.0.0.1:5500',
-      'http://localhost:5500',
-      'https://10.120.32.69:3000',
-    ],
+    origin: ['https://users.metropolia.fi'],
     credentials: true,
   })
 );
