bc95d6bf887d66be4ece332bb8ebab55505dcbbc
onnik <onni.kivinen@gmail.com>
2025-04-18 22:06:33 +0300

    Fix static file serving path: change '/public' to '/uploads' for serving static files

--- src/app.js
+++ src/app.js
@@ -15,7 +15,7 @@ app.use(
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
-app.use('/public', express.static('public'));
+app.use('/uploads', express.static('public'));
 
 app.get('/', (req, res) => {
   res.send('WEBPAGE REST API');
